027142720646b0e377237b459e31875f1fb2598f
marius02 <marius.morar02@gmail.com>
2024-06-22 03:10:04 -0700

    client: create dashboard page

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Dashboard from "../components/Dashboard";
+
+const Page = () => {
+  return(
+    <>
+      <div className="p-5">
+        <Dashboard />
+      </div>
+    </>
+  )
+}
+
+export default Page;
